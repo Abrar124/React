5ab617ff0a72ba6a2f6a8bb0afef24b4a46afedb
Abrar124 <abrar.khurshid.124@gmail.com>
2021-09-17 03:23:32 -0700

    adding and deleting feature complete

--- Todo-app/todo-app/src/todo.js
+++ Todo-app/todo-app/src/todo.js
@@ -1,18 +1,57 @@
 import "./App.css";
+import React, { useState } from "react";
+import TodoLists from "./todoList";
+
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Todo = () => {
+  const [inputList, SetInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    SetInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    SetInputList("");
+  };
+
+  const deleteItems = (id) => {
+    console.log("item is deleted");
+    setItems((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div className="main_div container">
       <div className="center_div">
         <br />
         <h1>Todo List</h1>
         <br />
-        <input type="text" placeholder="Your text here" />
-        <button>+</button>
-
+        <input
+          type="text"
+          placeholder="Your text here"
+          value={inputList}
+          onChange={itemEvent}
+        />
+        <button onClick={listOfItems}>+</button>
         <ol>
-          <li> Buy mango</li>
+          {items.map((itemVal, index) => {
+            return (
+              <TodoLists
+                key={index}
+                id={index}
+                text={itemVal}
+                onSelect={deleteItems}
+              />
+            );
+          })}
         </ol>
       </div>
     </div>
